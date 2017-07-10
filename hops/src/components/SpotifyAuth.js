bcc3cd1491ebe195d796c98c70fd6aff6ba7fea3
Keeley Hammond <keeleymhammond@gmail.com>
2017-07-10 14:04:04 -0700

    Rough spotify auth laid out

--- hops/src/components/SpotifyAuth.js
+++ hops/src/components/SpotifyAuth.js
@@ -1,13 +1,16 @@
 import { NativeModules } from 'react-native';
+const client_id = process.env.SPOTIFY_CLIENT_ID; 
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET; 
+const redirect_uri = process.env.SPOTIFY_REDIRECT_URI; 
 
 //Assign our module from NativeModules and assign it to a variable
-var SpotifyAuth = NativeModules.SpotifyAuth;
+const SpotifyAuth = NativeModules.SpotifyAuth;
 
 class yourComponent extends Component {
 	//Some code ...
 	someMethod(){
     //You need this to Auth a user, without it you cant use any method!
-		SpotifyAuth.setClientID('Your ClientId','Your redirectURL', ['streaming'], (error)=>{
+		SpotifyAuth.setClientID(client_id, redirect_uri, ['streaming'], (error)=>{
         if(error){
           //handle error
         } else {
